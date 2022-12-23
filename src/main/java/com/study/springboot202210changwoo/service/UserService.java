79210fbc6dc34d836c4a5c5d0f5452fad8d8ccfa
DevPCW <rorndksk123@naver.com>
2022-12-23 21:50:49 +0900

    저녁웹개발: Spring 데이터베이스 웹_changwoo

--- src/main/java/com/study/springboot202210changwoo/service/UserService.java
+++ src/main/java/com/study/springboot202210changwoo/service/UserService.java
@@ -2,8 +2,12 @@ package com.study.springboot202210changwoo.service;
 
 import com.study.springboot202210changwoo.repository.UserRepository;
 import com.study.springboot202210changwoo.web.dto.UserDto;
+import com.study.springboot202210changwoo.web.exception.CustomDuplicateUsernameException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -22,5 +26,14 @@ public class UserService {
         UserDto userDto = null;
         userDto = userRepository.findUserByUserId(userId);
         return userDto;
+    }
+
+    public void duplicateUsername(String username) {
+        UserDto userDto = userRepository.findUserByUsername(username);
+        if(userDto != null) {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put("duplicate", "이미 존재하는 사용자 이름입니다.");
+            throw new CustomDuplicateUsernameException("Duplicate username!!!", errorMap);
+        }
     }
 }
